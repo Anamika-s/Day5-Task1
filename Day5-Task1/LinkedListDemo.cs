da15985b299891c6bedbf8da3e2f1b4feba6f02c
Anamika Sawhney <anamika.sawhney22@gmail.com>
2024-04-19 16:33:13 +0530

    Add project files.

---
+++ Day5-Task1/LinkedListDemo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_Task1
+{
+    internal class LinkedListDemo
+    {
+        static void Main()
+        {
+            var names = new LinkedList<string>();
+            names.AddLast("Sonoo Jaiswal");
+            names.AddLast("Ankit");
+            names.AddLast("Peter");
+            names.AddLast("Irfan");
+            names.AddFirst("John");//added to first index
+            Queue<int> list = new Queue<int>();
+
+            PriorityQueue<int, int> queue = new System.Collections.Generic.PriorityQueue<int, int>();
+            queue.Enqueue(10, 10);
+            queue.Enqueue(20, 20);
+            queue.Enqueue(30, 3);
+
+
+            //queue.Dequeue();
+
+            while (queue.Count > 0)
+            {
+                var element = queue.Dequeue();
+                Console.WriteLine("Executing task: {0}", element);
+            }
+        }
+    }
+}
---
+++ Day5-Task1/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace Day5_Task1
+{
+    class Node
+    {
+        public int Data;
+        public Node next;
+    }
+
+    class CustomLinkedList
+    {
+        Node New, start = null, last, ptr, prev;
+        public CustomLinkedList(int data)
+        {
+            Node New = new Node();
+            New.Data = 0;
+            New.next = null;
+
+        }
+        public  void InsertAtBeginning(int data)
+        {
+            if (start == null)
+            {
+                New = new Node();
+                New.Data = data;
+                start = last = New;
+            }
+            else
+            {
+                New = new Node();
+                New.Data = data;
+                New.next = start;
+                start = New;
+            }
+
+        }
+        public void InsertAtEnd(int data)
+        {
+            Node New = new Node();
+            New.Data = data;
+            for (prev = ptr = start; ptr.next != null; prev = ptr, ptr = ptr.next) ;
+            prev.next = null;
+            last = prev;
+        }
+         public void InsertAtPosition(int position, int data)
+        {
+            Node New = new Node();
+            New.Data = data;
+            int i;
+            for (i = 0, prev = ptr = start;
+                ptr.next != null; prev = ptr, ptr = ptr.next, i++)
+            {
+                if(i==position)
+                {
+                    prev.next = New;
+                    New.next = ptr;
+                    break;
+                }
+            }
+
+        }
+        public void DeleteNode(int position)
+        {
+
+        }
+        public void UpdateNode(int data)
+        {
+        }
+        public void DisplayAllNodes()
+        {
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
